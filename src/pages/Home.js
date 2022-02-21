dbfe2e4b3566396444330651afedd80c40c95987
timppeli <91657796+timppeli@users.noreply.github.com>
2022-02-21 18:59:08 +0200

    Info text change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="container mt-3 text-center">
-      <p>This app gives you the name and a portrait for a Disney character as well as a list of all the appearances they have made in the Disneyverse.</p>
+      <p>This app gives you the name and a portrait of a random Disney character, and a list of all the appearances they have made in the Disneyverse.</p>
     </div>
   )
 }
