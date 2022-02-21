b915991dc1d8fce4f403b94dcb5cb5d0afd0a452
timppeli <91657796+timppeli@users.noreply.github.com>
2022-02-21 18:35:10 +0200

    Updated README

---
+++ README.md
@@ -0,0 +1,12 @@
+## React Exercise: Random Disney Character Data
+
+# Known bugs, grievances & "i-give-up"s
+- API: some image urls are broken > no image to display
+- API: some entries missing > sometimes you have to reroll to get a new character
+- no error messages for the grievances mentioned above
+- nor real error handling in general
+
+# Resources
+- VSCode
+- React
+- [Disneyapi](https://disneyapi.dev/docs)
--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
     return Math.floor(Math.random() * (CHARACTER_TOTAL - 0) + 0);
   }
 
-   useEffect(() => {
+  useEffect(() => {
     const CHARACTER_ID = getRandomCharacterId();
     axios.get(API_URL + CHARACTER_ID)
       .then((response) => {
@@ -44,8 +44,8 @@ function App() {
       }).catch(error => {
         setError(error)
         alert("An error occurred. Please try again.");
-    });
-  }, []) 
+      });
+  }, [])
 
   function showCharacter() {
     const CHARACTER_ID = getRandomCharacterId();
@@ -59,44 +59,47 @@ function App() {
         setShows(response.data.tvShows);
         setGames(response.data.videoGames);
         setParkAttractions(response.data.parkAttractions);
+      }).catch(error => {
+        setError(error)
+        alert("An error occurred. Please try again.");
       });
   }
 
   if (!isLoaded) {
     return (
       <div className="wrapper">
-      <Header />
-      <div className="container text-center p-3">
-        <button className="btn btn-primary" onClick={showCharacter}>Show another random character</button>
-      </div>
-      <div className="container">
-        Loading...
-      </div>
+        <Header />
+        <div className="container text-center p-3">
+          <button className="btn btn-primary" onClick={showCharacter}>Show another random character</button>
+        </div>
+        <div className="container">
+          Loading...
+        </div>
       </div>
     );
   } else {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Home />
-      <div className="container text-center p-3">
-        <button className="btn btn-primary" onClick={showCharacter}>Show another random character</button>
+    return (
+      <div className="wrapper">
+        <Header />
+        <Home />
+        <div className="container text-center p-3">
+          <button className="btn btn-primary" onClick={showCharacter}>Show another random character</button>
+        </div>
+        <div className="container d-flex flex-column align-items-start gap-3">
+          <CharacterDetails
+            characterName={characterName}
+            characterImage={characterImage}
+            imageAltText={imageAltText}
+            films={films}
+            shortFilms={shortFilms}
+            shows={shows}
+            games={games}
+            parkAttractions={parkAttractions}
+          />
+        </div>
       </div>
-      <div className="container d-flex flex-column align-items-start gap-3">
-        <CharacterDetails
-          characterName={characterName}
-          characterImage={characterImage}
-          imageAltText={imageAltText}
-          films={films}
-          shortFilms={shortFilms}
-          shows={shows}
-          games={games}
-          parkAttractions={parkAttractions}
-        />
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
