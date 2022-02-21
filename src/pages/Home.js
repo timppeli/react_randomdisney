6831fa3154f06e3d6d0fd269e0969267927c31ba
timppeli <91657796+timppeli@users.noreply.github.com>
2022-02-21 18:20:31 +0200

    Centered text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="container mt-3">
+    <div className="container mt-3 text-center">
       <p>This app gives you the name and a portrait for a Disney character as well as a list of all the appearances they have made in the Disneyverse.</p>
     </div>
   )
